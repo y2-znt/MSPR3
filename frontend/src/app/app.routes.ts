d1beb9bc8fa36b04359422a62f0430f319add2bb
y2_znt <yonideserbaix@gmail.com>
2025-04-10 09:16:36 +0200

    feat: migrate styles from CSS to SCSS

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent},
+  { path: '', component: DashboardComponent },
 
-
-
-    { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
