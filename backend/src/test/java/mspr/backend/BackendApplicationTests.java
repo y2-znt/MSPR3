53d951bd47f032eba1c863cda1eed62fe3e9297d
y2_znt <yonideserbaix@gmail.com>
2025-04-11 09:39:06 +0200

    refactor: remove dotenv setup from backend application tests to simplify test configuration

--- backend/src/test/java/mspr/backend/BackendApplicationTests.java
+++ backend/src/test/java/mspr/backend/BackendApplicationTests.java
@@ -1,21 +1,10 @@
 package mspr.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-	@BeforeAll
-	static void setup() {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-	}
-
 	@Test
 	void contextLoads() {
 	}
