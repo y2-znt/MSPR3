b70d4f64a79862a73b2af450b24550a7c54e686f
y2_znt <yonideserbaix@gmail.com>
2025-04-14 21:25:34 +0200

    feat: update getAllCountries method to support pagination with page and size parameters

--- frontend/src/app/services/country.service.ts
+++ frontend/src/app/services/country.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import { Country } from '../models/country.model';
+import { Page } from '../models/pagination.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,8 +14,13 @@ export class CountryService {
 
   constructor() {}
 
-  getAllCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.url}countries`);
+  getAllCountries(
+    page: number = 0,
+    size: number = 10
+  ): Observable<Page<Country>> {
+    return this.http.get<Page<Country>>(
+      `${this.url}countries?page=${page}&size=${size}`
+    );
   }
 
   getCountryById(id: number): Observable<Country> {
