922eb085776213a0f3cc59ed545e991c09977f31
alex <81435330+Al-vallon@users.noreply.github.com>
2025-05-22 23:38:30 +0200

    feat: add PredictComponent with routing and basic template; include unit tests

---
+++ frontend/src/app/components/predict/predict.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predict',
+  standalone: true,
+  imports: [],
+  templateUrl: './predict.component.html',
+  styleUrl: './predict.component.scss'
+})
+export class PredictComponent {
+
+}
